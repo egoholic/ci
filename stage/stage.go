7ce445d1f5ef0166fce872bf586f1293a4c2dfdd
Volodymyr Melnyk <vmelnyk@tradetracker.com>
2019-04-22 16:48:39 +0300

    updates stages

--- stage/stage.go
+++ stage/stage.go
@@ -1,31 +1,23 @@
 package stage
 
 import (
-	"context"
-	"io"
-	"os/exec"
-	"time"
-
 	"github.com/egoholic/ci/cmd"
 )
 
 type Stage struct {
-	name       string
-	commands   []cmd.Command // TODO: replace with custom command
-	stdOut     io.ReadCloser
-	stdErr     io.ReadCloser
-	startedAt  time.Time
-	finishedAt time.Time
+	name     string
+	commands []*cmd.Command
 }
 
-func New(name string, duration time.Duration) *Stage {
-	return &Stage{name, nil, nil, nil}
+func New(name string) *Stage {
+	commands := make([]*cmd.Command, 20)
+	return &Stage{name, commands}
 }
 
 func (stage *Stage) Name() string {
 	return stage.name
 }
 
-func (stage *Stage) AddCommand(ctx context.Context, name string, arg ...string) {
-	stage.commands = append(stage.commands, exec.CommandContext(ctx, name, arg...))
+func (stage *Stage) AddCommand(name string, arg ...string) {
+	stage.commands = append(stage.commands, cmd.New(name, arg...))
 }

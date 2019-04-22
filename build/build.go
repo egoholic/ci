11d63147af6e2ceb601d9c703faa4698b3a5e912
Vladimir Melnick <egotraumatic@gmail.com>
2019-04-22 08:58:29 +0300

    upd

--- build/build.go
+++ build/build.go
@@ -1,19 +1,29 @@
 package build
 
-import "time"
+import (
+	"time"
+
+	"github.com/egoholic/ci/stage"
+)
 
 type Build struct {
-	startedAt       time.Time
-	builtAt         time.Time
-	reference       string
-	currentStage    string
-	messagesTracked map[string][]string
+	uuid         string
+	reference    string
+	currentStage int
+	stages       []*stage.Stage
+	startedAt    time.Time
+	builtAt      time.Time
+	image        string
 }
 
 func New() *Build {
 	return &Build{}
 }
 
-func (build *Build) JSON() string {
-	return "" // TODO: implement
+func (build *Build) AddStage(buildStage *stage.Stage) {
+	build.stages = append(build.stages, buildStage)
+}
+
+func (build *Build) MessagesChannel() chan []string {
+	return build.messagesChannel
 }
--- stage/stage.go
+++ stage/stage.go
@@ -1,12 +1,33 @@
 package stage
 
-import "github.com/egoholic/ci/command"
+import (
+	"context"
+	"fmt"
+	"io"
+	"os/exec"
+	"time"
+)
 
 type Stage struct {
 	name     string
-	commands []*command.Command
+	commands []*exec.Cmd // TODO: replace with custom command
+	stdOut   io.ReadCloser
+	stdErr   io.ReadCloser
 }
 
-func New(name string) *Stage {
-	return &Stage{name, nil}
+func New(name string, duration time.Duration) *Stage {
+	return &Stage{name, nil, nil, nil}
+}
+
+func (stage *Stage) AddCommand(ctx context.Context, name string, arg ...string) {
+	stage.commands = append(stage.commands, exec.CommandContext(ctx, name, arg...))
+}
+
+func (stage *Stage) Run() {
+	ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+	defer cancel()
+	for i, cmd := range stage.commands {
+		fmt.Println("-(%d) Executing: `%#v`", i, cmd)
+		cmd.Run()
+	}
 }
